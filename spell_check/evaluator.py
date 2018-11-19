90be7d1d12bbd850c5d327cc92cc1b4172ad491f
Tzoutzoukos <petrosthm7@hotmail.com>
2018-11-19 19:33:33 +0200

    Evaluator correction

--- spell_check/evaluator.py
+++ spell_check/evaluator.py
@@ -7,7 +7,8 @@ fin = open("spell_checker_test_set.txt", 'r')
 # output to both STDIn and a file
 fout = open("evaluator_out.txt", 'w')
 
-correctCounter = 0
+word_level_correctCounter = 0
+letter_level_correctCounter = 0
 testSize = 0
 
 for line in fin:
@@ -20,20 +21,26 @@ for line in fin:
         testSize += 1
 
         # ./spell_check.sh gives several corrections
-        corrections = os.popen('./spell_check.sh' + ' ' + word).read().strip().split('\n')
-        print('\t' + word + ' -> ' + str(corrections))
-        fout.write('\t' + word + ' -> ' + str(corrections))
+        word_level_correction = os.popen('./spell_check.sh' + ' -w ' + word).read().strip()
+        letter_level_correction = os.popen('./spell_check.sh' + ' -l ' + word).read().strip()
+        print('\t-w' + word + ' -> ' + word_level_correction)
+        fout.write('\t-w' + word + ' -> ' + word_level_correction)
+        print('\t-w' + word + ' -> ' + letter_level_correction)
+        fout.write('\t-w' + word + ' -> ' + letter_level_correction)
 
         # if any correction matches the expected word increase counter
-        for correction in corrections:
-            if correction == targetWord:
-                correctCounter += 1
-                break
+        if targetWord == word_level_correction:
+            word_level_correctCounter += 1
+        if targetWord == letter_level_correction:
+            letter_level_correctCounter += 1
+            
     print()
     fout.write('\n')
 
-print(str(correctCounter) + ' correct out of ' + str(testSize) + ', ' + str(correctCounter / testSize * 100) + '%')
-fout.write(str(correctCounter) + ' correct out of ' + str(testSize) + ', ' + str(correctCounter / testSize * 100) + '%\n')
+print(str(word_level_correctCounter) + ' correct out of ' + str(testSize) + ', ' + str(word_level_correctCounter / testSize * 100) + '%')
+fout.write(str(word_level_correctCounter) + ' correct out of ' + str(testSize) + ', ' + str(word_level_correctCounter / testSize * 100) + '%')
+print(str(letter_level_correctCounter) + ' correct out of ' + str(testSize) + ', ' + str(letter_level_correctCounter / testSize * 100) + '%')
+fout.write(str(letter_level_correctCounter) + ' correct out of ' + str(testSize) + ', ' + str(letter_level_correctCounter / testSize * 100) + '%')
 
 fout.close()
 fin.close()
